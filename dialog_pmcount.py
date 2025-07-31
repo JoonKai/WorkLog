8b7831497b9c7cd1e8ea30ec0c600c3e95bc343a
Joonkai <joonseok831@gmail.com>
2025-08-01 07:07:28 +0900

    4444

--- dialog_pmcount.py
+++ dialog_pmcount.py
@@ -1,7 +1,12 @@
 from PySide6.QtWidgets import QDialog
+from ui.dialog.ui_pm_count import Ui_Dialog
 
 class Open_PMCount(QDialog):
     def __init__(self, parent=None):  # ✅ parent 인자를 받도록 수정
         super().__init__(parent)
-        self.resize(400, 300) 
-        self.setWindowTitle("Settings")
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.resize(600, 300) 
+        self.setWindowTitle("PM 카운트")
+
+    def savetable(table_widget, finename):
