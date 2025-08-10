cda2bc3e272eecaca9c26794ee786b548683d072
Joonkai <joonseok831@gmail.com>
2025-08-10 10:47:46 +0900

    1111

--- main_window.py
+++ main_window.py
@@ -5,6 +5,7 @@ from controller.widget_settings import OpenSettings
 from ui.windows.ui_main_window import Ui_MainWindow
 from controller.widget_schedule import SubScheduleForm
 from controller.widget_webcontrol import WebControl
+from controller.widget_datavisualize import OpenDataVisualize
 import mariadb
 
 
@@ -16,6 +17,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.mn_settings.triggered.connect(self.open_settings_dialog)
         self.mn_Schedule.triggered.connect(self.open_mdi_Schedule)
         self.mn_webControl.triggered.connect(self.open_web_control)
+        self.mn_datavisualize.triggered.connect(self.open_data_visualize)
         
     def open_settings_dialog(self):
         dlg = OpenSettings(self)
@@ -31,6 +33,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         sub = QMdiSubWindow()
         sub.setWidget(sub_widget)
         sub.setWindowTitle("PM 스케쥴")
+        sub.resize(800, 600)
+        sub.setAttribute(Qt.WA_DeleteOnClose, True)
+        self.mdiArea.addSubWindow(sub)
+        sub.show()
+
+    def open_data_visualize(self):
+        for sub in self.mdiArea.subWindowList():
+            if isinstance(sub.widget(), OpenDataVisualize):
+                sub.activateWindow()
+                self.mdiArea.setActiveSubWindow(sub)
+                return  
+        sub_widget = OpenDataVisualize(self)
+        sub = QMdiSubWindow()
+        sub.setWidget(sub_widget)
+        sub.setWindowTitle("데이터분석")
         sub.resize(800, 600)
         sub.setAttribute(Qt.WA_DeleteOnClose, True)
         self.mdiArea.addSubWindow(sub)
